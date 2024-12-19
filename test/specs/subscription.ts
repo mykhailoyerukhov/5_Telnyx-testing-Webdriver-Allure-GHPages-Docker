12762abc7ada59ea1ea9836b6d2435fd331a3348
mykhailoyerukhov <mykhailo.yerukhov@gmail.com>
2024-12-19 14:06:35 +0200

    update tests

--- test/specs/subscription.ts
+++ test/specs/subscription.ts
@@ -7,14 +7,14 @@ describe('Telnyx website testing',  () => {
             browser.pause(5000)
         })
 
-    it('subscribe on marketing newsletter. submit with no data', async () => {
+    it.skip('subscribe on marketing newsletter. submit with no data', async () => {
 
         await MainPageClass.ClickOnsubSubmitButton()
 
         const currentUrl = await browser.getUrl()
         await expect(currentUrl).toBe('https://telnyx.com/')
     })
-    it('subscribe on marketing newsletter. invalid data', async () => {
+    it.skip('subscribe on marketing newsletter. invalid data', async () => {
 
         await MainPageClass.InputsubEnterBusinessEmail('asdlkfjqewiorcas')
         await MainPageClass.ClickOnsubSubmitButton()
@@ -24,7 +24,7 @@ describe('Telnyx website testing',  () => {
         console.log('The message "Please include an @ in your email address appear, but it cant be traced, because it dynamically creates by JavaScript / AJAX and not shown in DOM elements.')
     })
   
-    it('subscribe on marketing newsletter with valid data', async () => {
+    it.skip('subscribe on marketing newsletter with valid data', async () => {
 
         await MainPageClass.InputsubEnterBusinessEmail('some_valid@email.com')
         await MainPageClass.ClickOnsubSubmitButton()
